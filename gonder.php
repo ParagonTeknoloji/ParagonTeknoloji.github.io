2f2b05371f72e5347b1de0f9114c74f3ad078fac
ParagonTeknoloji <64844003+ParagonTeknoloji@users.noreply.github.com>
2020-05-06 14:15:51 +0300

    Create gonder.php

---
+++ gonder.php
@@ -0,0 +1,87 @@
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+<?php
+ 
+$ad_soyad     = $_POST["ad_soyad"];
+$tel         = $_POST["tel"];
+$email         = $_POST["email"];
+$mesaj        = $_POST["mesaj"];
+$adres        = "paragonteknoloyi@gmail.com"; // Buraya e-postanin gonderilecegi mail adresini yaziniz
+$konu        = "Iletisim Formu";
+$tarih        = date('Y-m-d');
+$ip_adresi    = $_SERVER['REMOTE_ADDR'];
+ 
+if(($ad_soyad=="") or ($tel =="") or ($email=="") or ($mesaj=="")){
+ 
+echo "<center>Lutfen Ad Soyad, Telefon, E-Mail ve Mesaj alanlarini bos birakmayiniz.<br><a href=index.php>Geri don</a></center>";
+ 
+}
+else
+{
+ 
+$mesajveri.="ILETISIM FORMU MESAJI<br/><br/>";
+$mesajveri.="E-Mail:  ".$email."<br/>";
+$mesajveri.="Telefon:  ".$tel."<br/>";
+$mesajveri.="Tarih:  ".$tarih."<br/>";
+$mesajveri.="IP Adresi   :".$ip_adresi."<br/>";
+$mesajveri.="Mesaj:  ".$mesaj;
+ 
+$mesajyolla = mail($adres, $konu, $mesajveri, "Content-type: text/html; charset=utf-8\r\n");
+ 
+if($mesajyolla)
+{
+ 
+echo "<center>Iletisim mailiniz bize ulasti, en kisa surede cevaplanacaktir. Ilginiz icin tesekkur ederiz.<br><a href=index.php>Anasayfa</a></center>";
+ 
+}
+else
+{
+ 
+echo "<center>E-Mail gonderilirken hata olustu! Lutfen daha sonra tekrar deneyiniz.</center>";
+ 
+}
+}
+ 
+?>
